b42398af4e5796b77daf83836ab2272daf2bec1f
wyatt-drew <wyatt.j.drew@gmail.com>
2024-01-19 23:39:52 -0600

    added box around login

--- src/components/Login.js
+++ src/components/Login.js
@@ -38,30 +38,34 @@ function LoginPage() {
             <div className="user_forms-login">
               <h2 className="forms_title">Login</h2>
               <form className="forms_form">
-              <div className="forms_field">
-                <input type="text" placeholder="Username" required className="forms_field-input" />
-                </div>
-                <div className="forms_field">
-                    <input type="password" placeholder="Password" required className="forms_field-input" />
+                <div className="forms_fields">
+                    <div className="forms_field">
+                        <input type="text" placeholder="Username" required className="forms_field-input" />
+                    </div>
+                    <div className="forms_field">
+                        <input type="password" placeholder="Password" required className="forms_field-input" />
+                    </div>
                 </div>
                 <div className="forms_buttons">
                     <button type="submit" className="forms_buttons-action">Login</button>
                 </div>
-              </form>
+            </form>
             </div>
   
             {/* Signup Form */}
             <div className="user_forms-signup">
               <h2 className="forms_title">Sign Up</h2>
               <form className="forms_form">
-              <div className="forms_field">
-                <input type="text" placeholder="Username" required className="forms_field-input" />
-                </div>
-                <div className="forms_field">
-                    <input type="email" placeholder="Email" required className="forms_field-input" />
-                </div>
-                <div className="forms_field">
-                    <input type="password" placeholder="Password" required className="forms_field-input" />
+                <div className = "forms_fields">
+                    <div className="forms_field">
+                    <input type="text" placeholder="Username" required className="forms_field-input" />
+                    </div>
+                    <div className="forms_field">
+                        <input type="email" placeholder="Email" required className="forms_field-input" />
+                    </div>
+                    <div className="forms_field">
+                        <input type="password" placeholder="Password" required className="forms_field-input" />
+                    </div>
                 </div>
                 <div className="forms_buttons">
                     <button type="submit" className="forms_buttons-action">Sign Up</button>
