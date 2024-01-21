5005e395f5f53ce98fb71c18ac7e42c915f45a1c
wyatt-drew <wyatt.j.drew@gmail.com>
2024-01-20 21:35:09 -0600

    Improved nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,51 @@
-import React from 'react';
-import {Switch, Link, Route} from 'react-router-dom';
-import SiteNav, {ContentGroup} from 'react-site-nav'; // 1. Do this
-// import Home from './home';
-// import MyStory from './myStory';
+import React, {Component} from 'react';
+import {Switch, Link, Route, Redirect} from 'react-router-dom';
+import Home from '../pages/HomePage';
+// import SpaLink1 from './spaLink1';
+// import SpaLink2 from './spaLink2';
+import SiteNav, {ContentGroup} from 'react-site-nav';
+import './Navbar.css';
+import logo from '../assets/logo.png';
 
 export default () =>
   (
-    <div>
-      {/* 2. Add SiteNav with ContentGroup as children */}
-      <SiteNav>
-        <ContentGroup title="About" height="200">
-          {/* 3. You can add anything in a ContentGroup */}
-          <ul>
-            {/* react router link! */}
-            {/* <li><Link to="/my-story">My Story</Link></li> */}
-            <li>Another list item</li>
-          </ul>
-        </ContentGroup>
-        <ContentGroup title="Contact" height="200">
-          Free text followed by some links.<br/>
-          <a href="mailto:yusinto@gmail.com">Email</a><br/>
-          <a href="https://github.com/yusinto">Github</a>
-        </ContentGroup>
-      </SiteNav>
-      {/* <Switch> */}
-        {/* <Route exact path="/" component={Home}/> */}
-        {/* <Route path="/my-story" component={MyStory}/> */}
-      {/* </Switch> */}
-    </div>
+      <header>
+        {/* <div style={{width: '100%', background: 'white'}}>
+          <Link to="/">
+            <img style={{height: '70px', marginLeft: '20px', marginTop: '5px'}} src={logo}/>
+          </Link>
+        </div> */}
+        <SiteNav>
+            <ContentGroup title="About"/>
+            <ContentGroup title="Services" width="300" height="100">
+                        <ul>
+              <li><Link to="/spa-link-1">Corporate</Link></li>
+              <li><Link to="/spa-link-2">Private</Link></li>
+              <li><Link to="/spa-link-2">Weddings</Link></li>
+            </ul>
+            </ContentGroup>
+            <ContentGroup title="Testimonials"/>
+            <ContentGroup title="Team"/>
+            <ContentGroup title="Contact" width="200" height="150">
+            <ul style={{listStyleType: 'none'}}>
+              <li>
+                <a href="mailto:MyCorporateEmail@gmail.com">Email</a>
+              </li>
+              <li>
+                <a href="https://facebook.com/MyCorporateFacebook">Facebook</a>
+              </li>
+              <li>
+                <a href="https://twitter.com/MyCorporateTwitter">Twitter</a>
+              </li>
+              <li>
+                <a href="https://twitter.com/MyCorporateTwitter">Job Opportunities</a>
+              </li>
+            </ul>
+          </ContentGroup>
+            <ContentGroup title="About">
+          </ContentGroup>
+
+        </SiteNav>
+      </header>
   );
 
