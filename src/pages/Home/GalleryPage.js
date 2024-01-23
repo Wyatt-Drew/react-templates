3348fe8aea7939cc6c40a851842d9185a28ce720
wyatt-drew <wyatt.j.drew@gmail.com>
2024-01-22 19:46:31 -0600

    Added some gallery effects

--- src/pages/Home/GalleryPage.js
+++ src/pages/Home/GalleryPage.js
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import picture from '../../assets/farming.png';
 import picture2 from '../../assets/asperagus.jpg';
 import picture3 from '../../assets/grass.jpg';
 import picture4 from '../../assets/LoZ.gif';
 import './GalleryPage.css';
 
-const GalleryPage = () =>{
-    return(
+const images = [picture, picture2, picture3, picture4, picture2, picture3, picture, picture3, picture, picture4, picture2, picture];
+
+const GalleryPage = () => {
+    const imageRefs = useRef([]);
+  
+    useEffect(() => {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              entry.target.firstChild.classList.add('grow'); // Adding the class to the img element
+            }
+          });
+        },
+        { threshold: 0.1 }
+      );
+  
+      imageRefs.current.forEach((img) => {
+        if (img) observer.observe(img);
+      });
+  
+      return () => {
+        imageRefs.current.forEach((img) => {
+          if (img) observer.unobserve(img);
+        });
+      };
+    }, []);
+  
+    return (
         <div>
-            <div className = 'title'>
-                Gallery
-            </div>
-        
-            <div className = "image-gallery">
-                <img className = "image" src={picture} alt="Event" />
-                <img className = "image" src={picture2} alt="Event" />
-                <img className = "image" src={picture} alt="Event" />
-                <img className = "image" src={picture2} alt="Event" />
-                <img className = "image" src={picture3} alt="Event" />
-                <img className = "image" src={picture4} alt="Event" />
-                <img className = "image" src={picture} alt="Event" />
-                <img className = "image" src={picture3} alt="Event" />
-
-                <img className = "image" src={picture} alt="Event" />
-                <img className = "image" src={picture2} alt="Event" />
-                <img className = "image" src={picture4} alt="Event" />
-
-            </div>
+          <div className='title'>Gallery</div>
+          <div className="image-gallery">
+            {images.map((image, index) => (
+              <div 
+                key={index} 
+                ref={el => imageRefs.current[index] = el}
+                className="image-wrapper"
+              >
+                <img
+                  alt={'Event'}
+                  src={image}
+                  className="image"
+                  id={`img-${index}`}
+                />
+                <div className="overlay">
+                  Image Description
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-    );
-}
-export default GalleryPage;
+      );
+  };
+    
+  export default GalleryPage;
